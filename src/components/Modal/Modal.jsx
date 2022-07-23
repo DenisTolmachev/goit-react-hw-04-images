51551bdc00198d16ab118bfac43c13f8d7821690
Denis <denis.tolmachev@gmail.com>
2022-07-23 20:44:10 +0300

    refactoring modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -29,30 +29,3 @@ export const Modal = ({modalImg, onClick}) => {
     </ModalBackdrop>
   );
 };
-
-// export class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.closeModal);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.closeModal);
-//   }
-
-//   closeModal = e => {
-//     if (e.key === 'Escape' || e.target === e.currentTarget) {
-//       this.props.onClick();
-//     }
-//   };
-
-//   render() {
-//     const { modalImg } = this.props;
-//     return (
-//       <ModalBackdrop onClick={this.closeModal}>
-//         <div>
-//           <ModalImg src={modalImg} alt="" />
-//         </div>
-//       </ModalBackdrop>
-//     );
-//   }
-// }
