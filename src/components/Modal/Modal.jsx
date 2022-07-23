bff1b075261fe7bbdf3f676bbf6610d36802ea80
Denis <denis.tolmachev@gmail.com>
2022-07-23 20:35:53 +0300

    refactoring modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,29 +1,58 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { ModalBackdrop, ModalImg } from './Modal.styled';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.closeModal);
-  }
+export const Modal = ({modalImg, onClick}) => {
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeModal);
-  }
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.key === 'Escape') {
+        onClick();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClick]);
 
-  closeModal = e => {
-    if (e.key === 'Escape' || e.target === e.currentTarget) {
-      this.props.onClick();
+  const closeModal = e => {
+    if (e.target === e.currentTarget) {
+      onClick();
     }
   };
 
-  render() {
-    const { modalImg } = this.props;
-    return (
-      <ModalBackdrop onClick={this.closeModal}>
-        <div>
-          <ModalImg src={modalImg} alt="" />
-        </div>
-      </ModalBackdrop>
-    );
-  }
-}
+  return (
+    <ModalBackdrop onClick={closeModal}>
+      <div>
+        <ModalImg src={modalImg} alt="" />
+      </div>
+    </ModalBackdrop>
+  );
+};
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.closeModal);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.closeModal);
+//   }
+
+//   closeModal = e => {
+//     if (e.key === 'Escape' || e.target === e.currentTarget) {
+//       this.props.onClick();
+//     }
+//   };
+
+//   render() {
+//     const { modalImg } = this.props;
+//     return (
+//       <ModalBackdrop onClick={this.closeModal}>
+//         <div>
+//           <ModalImg src={modalImg} alt="" />
+//         </div>
+//       </ModalBackdrop>
+//     );
+//   }
+// }
