c5f4bf94689d4f1e34c8ccfc5890fc879ed6fcc6
Denis <denis.tolmachev@gmail.com>
2022-07-23 21:23:51 +0300

    refactoring ImageGalleryItem code

--- src/components/ImageGallery/Item/ImageGalleryItem.jsx
+++ src/components/ImageGallery/Item/ImageGalleryItem.jsx
@@ -27,29 +27,3 @@ const toggleModal = (_) => {
     </ImageGalleryItemItem>
   );
 };
-
-// export class ImageGalleryItem extends Component {
-//   state = {
-//     isOpen: false,
-//   };
-
-//   toggleModal = () => {
-//     this.setState(prevState => ({ isOpen: !prevState.isOpen }));
-//   };
-
-//   render() {
-//     const { item } = this.props;
-//     return (
-//       <ImageGalleryItemItem>
-//         <ImageGalleryItemImage
-//           src={item.webformatURL}
-//           alt=""
-//           onClick={this.toggleModal}
-//         />
-//         {this.state.isOpen && (
-//           <Modal onClick={this.toggleModal} modalImg={item.largeImageURL} />
-//         )}
-//       </ImageGalleryItemItem>
-//     );
-//   }
-// }
