58a59b92ba6fbfadcac0e392b1d980a2c6e0c704
Denis <denis.tolmachev@gmail.com>
2022-07-23 21:23:09 +0300

    refactoring ImageGalleryItem code

--- src/components/ImageGallery/Item/ImageGalleryItem.jsx
+++ src/components/ImageGallery/Item/ImageGalleryItem.jsx
@@ -3,30 +3,53 @@ import {
   ImageGalleryItemItem,
   ImageGalleryItemImage,
 } from './ImageGalleryItem.styled';
-import { Component } from 'react';
+//import { Component } from 'react';
+import { useState } from 'react';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isOpen: false,
-  };
+export const ImageGalleryItem = ({item}) => {
 
-  toggleModal = () => {
-    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
-  };
+const [isOpen, setIsOpen] = useState(false);
 
-  render() {
-    const { item } = this.props;
-    return (
-      <ImageGalleryItemItem>
-        <ImageGalleryItemImage
-          src={item.webformatURL}
-          alt=""
-          onClick={this.toggleModal}
-        />
-        {this.state.isOpen && (
-          <Modal onClick={this.toggleModal} modalImg={item.largeImageURL} />
-        )}
-      </ImageGalleryItemItem>
-    );
-  }
+const toggleModal = (_) => {
+  setIsOpen(prevState => !isOpen);
 }
+
+  return (
+    <ImageGalleryItemItem>
+      <ImageGalleryItemImage
+        src={item.webformatURL}
+        alt=""
+        onClick={toggleModal}
+      />
+      {isOpen && (
+        <Modal onClick={toggleModal} modalImg={item.largeImageURL} />
+      )}
+    </ImageGalleryItemItem>
+  );
+};
+
+// export class ImageGalleryItem extends Component {
+//   state = {
+//     isOpen: false,
+//   };
+
+//   toggleModal = () => {
+//     this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+//   };
+
+//   render() {
+//     const { item } = this.props;
+//     return (
+//       <ImageGalleryItemItem>
+//         <ImageGalleryItemImage
+//           src={item.webformatURL}
+//           alt=""
+//           onClick={this.toggleModal}
+//         />
+//         {this.state.isOpen && (
+//           <Modal onClick={this.toggleModal} modalImg={item.largeImageURL} />
+//         )}
+//       </ImageGalleryItemItem>
+//     );
+//   }
+// }
